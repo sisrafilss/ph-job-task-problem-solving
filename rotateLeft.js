263c74ebe9c134223be67408b3700cc93798180e
Israfil Hossen <sisrafilss@gmail.com>
2023-01-29 07:25:35 +0600

    function implimentation completed

---
+++ rotateLeft.js
@@ -0,0 +1,25 @@
+// Test Data
+const array = [1, 2]; 
+const rotation = 1;
+
+// Testing ->
+console.log(`Before Rotation the Array is:`, array);
+// function call
+rotateLeft(array, rotation);
+console.log(`After ${rotation} Left Rotation the Array is:`, array);
+
+
+
+// left rotate the array element rotation (rotation is the input) times
+function rotateLeft(array, rotation) {
+    const len = array.length;
+    let actualRotation = rotation % len;
+
+    for (let i = 0; i < actualRotation; i++) {
+        let tmp = array[0];
+        for (let j = 0; j < len - 1; j++) {
+            array[j] = array[j+1];
+        }
+        array[len-1] = tmp;
+    }
+}
